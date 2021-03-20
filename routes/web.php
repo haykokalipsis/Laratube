8447a04786e74059aa64f42c5338fe55bcd0e7db
haykokalipsis <haykokalipsis@gmail.com>
2021-03-20 16:04:13 +0400

    Update to laravel 7

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::get('test', function() {
 //    Storage::disk('google')->rename($files[0], 'New Name');
 //    $response = Storage::disk('google')->download($files[0], 'file.jpg'); $response->send();
 //    Storage::disk('dropbox')->makeDirectory('New Directory');
-    dd(Storage::disk('dropbox')->allFiles());
-//    dd(Storage::disk('dropbox')->put('LaraTube', 'Hello World'));
+//    dd(Storage::disk('dropbox')->allFiles());
+    dd(Storage::disk('dropbox')->put('LaraTube', 'Hello World'));
 });
 
 Auth::routes();
